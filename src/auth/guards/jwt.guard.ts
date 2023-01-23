38572ab459a7f9431ab0f37f616088f4cf64790b
Ziya <ziyakerimli04@gmail.com>
2023-01-23 13:28:12 +0400

    [update] add public decorator

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,9 +1,17 @@
 import {AuthGuard} from "@nestjs/passport";
 import {ExecutionContext, UnauthorizedException} from "@nestjs/common";
 import {Observable} from "rxjs";
+import {Reflector} from "@nestjs/core";
+import {IS_PUBLIC_KEY} from "../decorators/public.decorator";
 
 export class JwtGuard extends AuthGuard('jwt'){
+    constructor(private reflector: Reflector) {
+        super();
+    }
+
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY,[context.getHandler, context.getClass]);
+        if(isPublic) return true;
         return super.canActivate(context);
     }
 
