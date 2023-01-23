730321e2d73b43a4d6c96fae9af5e7d66211ace5
Ziya <ziyakerimli04@gmail.com>
2023-01-23 12:33:55 +0400

    [refactor] fix path

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import {JwtModule} from "@nestjs/jwt";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {JwtModule} from "@nestjs/jwt";
 import {AccessTokenConfig} from "../configs/jwt.config";
-import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
     imports: [
