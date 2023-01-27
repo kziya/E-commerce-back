ca2c276861fe2d35a8294f8a8735a51a2f8cfb2d
Ziya <ziyakerimli04@gmail.com>
2023-01-27 10:25:10 +0400

    [update] add husky

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import envConfig from "./configs/env.config";
+import envConfig from './configs/env.config';
 
 @Module({
-  imports: [
-      ConfigModule.forRoot(envConfig()),
-  ],
+  imports: [ConfigModule.forRoot(envConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
