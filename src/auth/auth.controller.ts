d9cb31200736261c3822fc24e76ebf1ce393de01
Ziya <ziyakerimli04@gmail.com>
2023-01-30 13:41:03 +0400

    [refactoring] fix route bugs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,19 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { SignUpDto } from './dtos/sign-up.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Public()
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return loginDto;
   }
 
+  @Public()
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
     return signUpDto;
