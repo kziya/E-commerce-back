813ec3280d646ca59fc31ce7882ab1ed8e38c316
Ziya <ziyakerimli04@gmail.com>
2023-01-30 16:33:09 +0400

    [update] make bcrypt.service function

--- src/bcrypt/bcrypt.service.ts
+++ src/bcrypt/bcrypt.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class BcryptService {
   constructor(private readonly configService: ConfigService) {}
+  async hash(password): Promise<string> {
+    return bcrypt.hash(password, Number(this.configService.get('BCRYPT_SALT')));
+  }
+
+  async verify(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
 }
