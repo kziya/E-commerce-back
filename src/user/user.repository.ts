60f282c0fd4a267e17f2092999a625101d639c45
Ziya <ziyakerimli04@gmail.com>
2023-01-30 17:36:47 +0400

    [update] make user.repository functions

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -7,13 +7,15 @@ import { IUserCreate } from './user.types';
 @Injectable()
 export class UserRepository {
   constructor(private readonly prismaService: PrismaService) {}
-  async findOneById(id: number) {
-    return this.prismaService.user.findFirst({ where: { id } });
-  }
-
-  async create(user: IUserCreate): Promise<user | object> {
+  async create(user: IUserCreate): Promise<user> {
     return this.prismaService.user.create({
       data: user,
     });
+  }
+  async findOneById(id: number) {
+    return this.prismaService.user.findFirst({ where: { id } });
+  }
+  async findOneByEmail(email: string) {
+    return this.prismaService.user.findUnique({ where: { email } });
   }
 }
