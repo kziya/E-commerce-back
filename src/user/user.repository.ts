b0b909a066100c43bfe6d40ecd12e78906267f78
Ziya <ziyakerimli04@gmail.com>
2023-01-30 17:34:34 +0400

    [update] add user.types file

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { user } from '@prisma/client';
 
 import { PrismaService } from '../prisma/prisma.service';
+import { IUserCreate } from './user.types';
 
 @Injectable()
 export class UserRepository {
   constructor(private readonly prismaService: PrismaService) {}
   async findOneById(id: number) {
     return this.prismaService.user.findFirst({ where: { id } });
+  }
+
+  async create(user: IUserCreate): Promise<user | object> {
+    return this.prismaService.user.create({
+      data: user,
+    });
   }
 }
---
+++ src/user/user.types.ts
@@ -0,0 +1,6 @@
+export interface IUserCreate {
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  password: string;
+}
