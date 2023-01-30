3f1ec06cf7e8b360d4d88898fb97cd0f0d2927c5
Ziya <ziyakerimli04@gmail.com>
2023-01-30 13:14:25 +0400

    [update] fix login.dto && add sign-up.dto

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, Length } from 'class-validator';
+import {IsEmail, IsNotEmpty, Length} from 'class-validator';
 
 import ErrorMessagesEnum from '../enums/error-messages.enum';
 
 export class LoginDto {
+  @IsNotEmpty({ message: ErrorMessagesEnum.canNotBeEmpty })
   @IsEmail({}, { message: ErrorMessagesEnum.emailNotValid })
   email: string;
 
+  @IsNotEmpty({ message: ErrorMessagesEnum.canNotBeEmpty })
   @Length(8, 20, { message: ErrorMessagesEnum.passwordLength })
   password: string;
 }
--- src/auth/enums/error-messages.enum.ts
+++ src/auth/enums/error-messages.enum.ts
@@ -1,4 +1,5 @@
 enum ErrorMessagesEnum {
+  canNotBeEmpty = 'Can not be Empty!',
   emailNotValid = 'Email is not Valid !',
   passwordLength = 'Password length must be less than 20 and longer than 13',
 }
