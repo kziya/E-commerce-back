543cf474ac3f5f523d13d7b253618ff86b44adf9
Ziya <ziyakerimli04@gmail.com>
2023-01-30 13:25:21 +0400

    [update] linter & prettier & add new routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
+import { SignUpDto } from './dtos/sign-up.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return loginDto;
+  }
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return signUpDto;
+  }
+}
--- src/auth/dtos/sign-up.dto.ts
+++ src/auth/dtos/sign-up.dto.ts
@@ -1,15 +1,15 @@
-import {IsEmail, IsNotEmpty, Length} from "class-validator";
-import ErrorMessagesEnum from "../enums/error-messages.enum";
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+import ErrorMessagesEnum from '../enums/error-messages.enum';
 
-export class SignUpDto{
-    firstName?: string;
-    lastName?: string;
+export class SignUpDto {
+  firstName?: string;
+  lastName?: string;
 
-    @IsNotEmpty({ message: ErrorMessagesEnum.canNotBeEmpty })
-    @IsEmail({}, { message: ErrorMessagesEnum.emailNotValid })
-    email: string;
+  @IsNotEmpty({ message: ErrorMessagesEnum.canNotBeEmpty })
+  @IsEmail({}, { message: ErrorMessagesEnum.emailNotValid })
+  email: string;
 
-    @IsNotEmpty({ message: ErrorMessagesEnum.canNotBeEmpty })
-    @Length(8, 20, { message: ErrorMessagesEnum.passwordLength })
-    password: string;
+  @IsNotEmpty({ message: ErrorMessagesEnum.canNotBeEmpty })
+  @Length(8, 20, { message: ErrorMessagesEnum.passwordLength })
+  password: string;
 }
