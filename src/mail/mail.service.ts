16652304dc92d4b1911e6dafa5ea869fd08b61ee
Ziya <ziyakerimli04@gmail.com>
2023-02-10 17:18:31 +0400

    [update] make sendVerificationUrl method

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
+import { user } from '@prisma/client';
 
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
+
+  async sendVerificationUrl(User: user) {
+    return this.mailerService.sendMail({
+      to: User.email,
+      subject: 'Verify Your account',
+      template: 'verify-account',
+      context: {
+        user: User,
+      },
+    });
+  }
 }
